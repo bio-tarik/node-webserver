65a4231d4c2a9eb609de61276f06ab709ace2ef1
bio-tarik <bio.tarik@gmail.com>
2018-06-13 17:24:06 -0300

    Setting up some basic handlebars rendering

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 let app = express();
 
+app.set('view engine', 'hbs');
+app.use(express.static(`${__dirname}/public`));
+
 app.get('/', (req, res) => {
-    res.send({
-        name: 'Tarik',
-        likes: [
-            'travelling',
-            'biking',
-            'studying'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my Website',
+        currentYear: new Date().getFullYear()
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
@@ -23,4 +27,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is up and running on port 3000');
+});
